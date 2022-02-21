381ab023931bebe65b0541e6c9b45f763606c2ad
Rahul <rahul.kumawat@siemens.com>
2022-02-21 10:54:18 +0530

    aspect ratio responsive

--- test.js
+++ test.js
@@ -2,16 +2,13 @@ window.onload = () => {
     for (let elem of document.querySelectorAll(".skills-container > .skills-palatte > div")) {
         elem.addEventListener("mousedown", event => {
             const selected = event.currentTarget;
-            console.log(selected);
             for (let e of selected.parentElement.children) {
                 if (e!= selected && e.classList.contains("selected")) {
                     e.classList.remove("selected");
                     break;
                 }
             }
-            //if(selected.classList.contains("selected")){
-                selected.classList.toggle("selected");
-            //}
+            selected.classList.toggle("selected");
         });
     }
 }
