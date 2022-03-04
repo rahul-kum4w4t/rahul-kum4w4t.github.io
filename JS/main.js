ece77758d9613d262c04b101d8341384078956f1
Zero15 <rahul.m07xc@gmail.com>
2022-03-04 18:08:10 +0530

    1st release

--- JS/main.js
+++ JS/main.js
@@ -1,28 +1,9 @@
 import TypeWriterEffect from './typewriter-effect.js';
 
 export default function onLoad() {
-    new TypeWriterEffect(
-        document.getElementsByClassName("intro")[0],
-        { cursorColor: "white", wait: 0 }
-    ).startTyping().then(() => {
-        return new TypeWriterEffect(
-            document.getElementsByClassName("intro-about")[0],
-            { cursorColor: "white", ms: 70, wait: 0 }
-        ).startTyping();
-    }).then(() => {
-        return new TypeWriterEffect(
-            document.getElementsByClassName("intro-summary-1")[0],
-            { cursorColor: "white", ms: 50, wait: 0 }
-        ).startTyping();
-    }).then(() => {
-        return new TypeWriterEffect(
-            document.getElementsByClassName("intro-summary-2")[0],
-            { cursorColor: "white", ms: 50, wait: 0  }
-        ).startTyping();
-    }).then(() => {
-        document.querySelector(".contact-me").style.opacity = 1;
-    });
-
+    /**
+     * Binding events
+    */
     for (let elem of document.querySelectorAll(".skills-container > .skills-palatte > div")) {
         elem.addEventListener("mousedown", event => {
             const selected = event.currentTarget;
@@ -78,107 +59,17 @@ export default function onLoad() {
         }
     });
 
-    function navigateAndClear(elem, items, up, down) {
-        
-        elem.classList.add("selected");
-        const elemToDisplay = document.querySelector(`.${elem.dataset.navigate}`);
-        for(let elem of elemToDisplay.parentElement.children){
-            if(elem.classList.contains("selected")) {
-                elem.classList.remove("selected");
-            }
-        }
-        elemToDisplay.classList.add("selected");
-        
-        if (elem == items[0]) {
-            up.classList.add("bound");
-            if (down.classList.contains("bound")) {
-                down.classList.remove("bound");
-            }
-        } else if (elem == items[items.length - 1]) {
-            down.classList.add("bound");
-            if (up.classList.contains("bound")) {
-                up.classList.remove("bound");
-            }
-        } else {
-            if (up.classList.contains("bound")) {
-                up.classList.remove("bound");
-            }
-            if (down.classList.contains("bound")) {
-                down.classList.remove("bound");
-            }
-        }
-    }
-
-
-    window.scrollTo(0, 0);
-
 
     document.querySelector(".contact-me").addEventListener("mousedown", e => {
         document.querySelector(".side-menu-item[data-navigate=contact-info]").dispatchEvent(new Event("mousedown",{bubbles: true}));
     });
 
-    /*let lastScrollPos = 0;
-    document.body.onscroll = event => {
-        if(scrollAllowed){
-            console.log(scrollAllowed);
-            scrollAllowed = false;
-            let st = window.pageYOffset || document.documentElement.scrollTop;
-            if(st > lastScrollPos){
-                document.querySelector(".side-menu-down").dispatchEvent(new Event("mousedown",{bubbles: true}));
-            }else{
-                document.querySelector(".side-menu-up").dispatchEvent(new Event("mousedown",{bubbles: true}));
-            }
-            lastScrollPos =  st;
-        }
-    }*/
-
-    /*document.querySelector(".nav-menu > .pane_1 > i").addEventListener("mouseup", event => {
-
-        const target = event.target.parentElement.parentElement;
-        target.classList.remove("closed");
-        target.classList.add("open");
-    });
-
-    document.querySelector(".nav-menu > div#close > img").addEventListener("mouseup", event => {
-
-        const target = event.target.parentElement.parentElement;
-        target.classList.remove("open");
-        target.classList.add("closed");
-    });
-
-    document.querySelector(".nav-menu > .pane_3 > #skills").addEventListener("mousedown", () => {
-        document.querySelector(".nav-menu > div#close > img").dispatchEvent(new Event("mouseup"));
-        document.querySelector(".card-container").scrollIntoView();
-        
-    });
-
-    document.querySelector(".nav-menu > .pane_3 > #about").addEventListener("mousedown", () => {
-        document.querySelector(".nav-menu > div#close > img").dispatchEvent(new Event("mouseup"));
-        document.querySelector(".dashboard").scrollIntoView();
-    });
-
-    document.querySelector(".nav-menu > .pane_3 > #experience").addEventListener("mousedown", () => {
-        document.querySelector(".nav-menu > div#close > img").dispatchEvent(new Event("mouseup"));
-        document.querySelector(".experience-container").scrollIntoView();
-    });*/
-
-
-    /*for (let card of document.querySelectorAll(".card-container > div.card-deck > div")) {
-        card.addEventListener("mousedown", drawCard);
-    }
-
-    document.querySelector(".card-container > div.card-deck").addEventListener("mouseenter", spreadCards);
-
-    document.querySelector(".card-container > div.card-deck").addEventListener("mouseleave", delayedStackUpdate);*/
-
     window.addEventListener('resize', resizeDropBar);
-    resizeDropBar();
 
     for (let tab of document.querySelectorAll(".experience > div:nth-child(2) > span")) {
         tab.addEventListener("mousedown", event => {
             const tab = event.currentTarget;
             const index = parseInt(tab.dataset.index);
-            console.log(index);
 
             const parent = tab.parentElement;
             const grandParent = tab.parentElement.parentElement;
@@ -202,85 +93,131 @@ export default function onLoad() {
             }
         });
     }
+
+    if (!("ontouchstart" in document.documentElement))
+    {
+        window.addEventListener('resize', resizeEffects, true);
+        resizeEffects();
+    }
+
+    const boxes = document.querySelectorAll(".cross-line > div");
+
+    for(let box of boxes){
+        box.addEventListener("mousedown", ({currentTarget}) => {
+            for(let child of currentTarget.parentElement.children){
+                if(child.classList.contains("selected")){
+                    child.classList.remove("selected");
+                    break;
+                }
+            }
+            currentTarget.classList.add("selected");
+            const id = currentTarget.dataset.id;
+
+            let grandParentChildren = currentTarget.parentElement.parentElement.children;
+            for(let i = 1; i < grandParentChildren.length; i++){
+                if(grandParentChildren[i].classList.contains("selected")){
+                    grandParentChildren[i].classList.remove("selected");
+                }
+
+                if(grandParentChildren[i].dataset.id == id){
+                    grandParentChildren[i].classList.add("selected");
+                }
+            }
+        });
+    }
+
+
+    /**
+     * Page initialization tasks
+    */
+
+    window.scrollTo(0, 0);
+
+    resizeDropBar();
+
+    // Type writer effect
+    new TypeWriterEffect(
+        document.getElementsByClassName("intro")[0],
+        { cursorColor: "white", wait: 0 }
+    ).startTyping().then(() => {
+        return new TypeWriterEffect(
+            document.getElementsByClassName("intro-about")[0],
+            { cursorColor: "white", ms: 70, wait: 0 }
+        ).startTyping();
+    }).then(() => {
+        return new TypeWriterEffect(
+            document.getElementsByClassName("intro-summary-1")[0],
+            { cursorColor: "white", ms: 50, wait: 0 }
+        ).startTyping();
+    }).then(() => {
+        return new TypeWriterEffect(
+            document.getElementsByClassName("intro-summary-2")[0],
+            { cursorColor: "white", ms: 50, wait: 0  }
+        ).startTyping();
+    }).then(() => {
+        document.querySelector(".contact-me").style.opacity = 1;
+    });
 }
 
 function resizeDropBar() {
-    const dropContainers = document.querySelectorAll(".drop-container");
-    for (const cont of dropContainers) {
-        if (cont.dataset.direction == "right") {
-            cont.style.right = -2 * (cont.offsetWidth / 5) + "px";
-        } else {
-            cont.style.left = -2 * (cont.offsetWidth / 5) + "px";
-        }
+    const dropContainer = document.querySelector(".drop-container");
+    dropContainer.style.left = -2 * (dropContainer.offsetWidth / 5) + "px";
+}
+
+function resizeEffects() {
+    const width = document.documentElement.offsetWidth;
+    const height = document.documentElement.offsetHeight;
+    let ratio = width / height;
+    let length = Math.sqrt(width ** 2 + height ** 2);
+    let angle = Math.atan(ratio) * 180 / Math.PI;
+    //console.log(width + ", " + height + "\n" + length + " : " + angle);
+
+    let line  = document.querySelector(".cross-line");
+    line.style.height = (length-10)+"px";
+    line.style.transform = `rotate(${angle}deg)`;
+
+    let boxes  = document.querySelectorAll(".cross-line > div");
+    for(let box of boxes){
+        box.style.transform = `rotate(${-1 * angle}deg)`
     }
 }
 
-/*let cardsOpen = false;
-function spreadCards({ currentTarget: { children: cards } }) {
-
-    const margin = 40;
-    const minMargin = 15;
-    let count = 1;
-    const windowWidthUsable = document.documentElement.clientWidth - margin;
-    const distributionWidth = windowWidthUsable - cards[0].clientWidth;
-    const remainingWidthForEachChild = Math.round(distributionWidth / cards.length);
-
-    if (remainingWidthForEachChild >= minMargin) {
-        let topOne = null;
-        for (let elem of cards) {
-            if (!elem.classList.contains("restack")) {
-                elem.style.left = `${remainingWidthForEachChild * count++}px`;
-                elem.style.transform = "rotateZ(-10deg) rotateX(30deg) rotateY(-10deg)";
-            } else {
-                topOne = elem;
-            }
-        }
-        if (topOne) {
-            topOne.style.left = `${remainingWidthForEachChild * cards.length}px`;
-            topOne.style.transform = "rotateZ(-10deg) rotateX(30deg) rotateY(-10deg)";
+function navigateAndClear(elem, items, up, down) {
+        
+    elem.classList.add("selected");
+    const elemToDisplay = document.querySelector(`.${elem.dataset.navigate}`);
+    for(let elem of elemToDisplay.parentElement.children){
+        if(elem.classList.contains("selected")) {
+            elem.classList.remove("selected");
         }
     }
-    cardsOpen = true;
-}
-
-function delayedStackUpdate({ currentTarget: { children } }) {
-    setTimeout(stackCards, 100, children);
-}
-
-function stackCards(cards) {
-    for (let elem of cards) {
-        elem.style.left = null;
-        elem.style.transform = null;
-    }
-    cardsOpen = false;
-}
-
-drawCard.allow = true;
-const DELAY = 1500;
-function drawCard(event) {
-
-    if (drawCard.allow) {
-        drawCard.allow = false;
-        const { currentTarget: clickedCard, currentTarget: { parentElement: { children: cards } } } = event;
-
-        if (!clickedCard.classList.contains('restack')) {
-            clickedCard.classList.add("remove");
-            setTimeout(() => {
-
-                for (let card of cards) {
-                    if (card !== clickedCard && card.classList.contains("restack")) {
-                        card.classList.remove("restack");
-                        card.classList.remove("remove");
-                    }
-                }
-                clickedCard.classList.add("restack");
-                drawCard.allow = true;
-                spreadCards({ currentTarget: { children: cards } });
-            }, DELAY);
-        } else {
-            if (cardsOpen) stackCards(cards);
-            else spreadCards({ currentTarget: { children: cards } });
-            drawCard.allow = true;
+    elemToDisplay.classList.add("selected");
+    
+    if (elem == items[0]) {
+        up.classList.add("bound");
+        if (down.classList.contains("bound")) {
+            down.classList.remove("bound");
+        }
+    } else if (elem == items[items.length - 1]) {
+        down.classList.add("bound");
+        if (up.classList.contains("bound")) {
+            up.classList.remove("bound");
+        }
+    } else {
+        if (up.classList.contains("bound")) {
+            up.classList.remove("bound");
+        }
+        if (down.classList.contains("bound")) {
+            down.classList.remove("bound");
         }
     }
-}*/
+
+    const sideMenu = document.querySelector(".side-menu");
+    if(elemToDisplay.classList.contains("experience-container")){
+        sideMenu.classList.add("left");
+        sideMenu.classList.remove("right");
+    }else if(sideMenu.classList.contains("left")){
+        sideMenu.classList.remove("left");
+        sideMenu.classList.add("right");
+    }
+}
